4410676d2b32c07bacfbb0cd73f19e02fc99777b
Darshan Godase <darshangodase10@example.com>
2025-03-01 18:21:08 +0530

    Included backend for auth and connect with frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { AnimatePresence } from "framer-motion";
 import "react-toastify/dist/ReactToastify.css";
 import LoginForm from "./pages/Login";
 import Signup from "./pages/Signup"
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import Dashboard from "./pages/Dashboard";
+import Preloader from "./components/Preloader";
+import { AuthProvider } from "./context/AuthContext";
+
 function App() {
-  return <>
-  <Router>
-       <ToastContainer position="top-right" autoClose={3000} />
-       <Header/>
-       <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-  </Router>
-  </>;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate initial loading or do actual data fetching
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <AnimatePresence mode='wait'>
+        {loading && <Preloader setLoading={setLoading} />}
+      </AnimatePresence>
+      
+      {!loading && (
+        <AuthProvider>
+          <Router>
+            <ToastContainer position="top-right" autoClose={3000} />
+            <Header />
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/login" element={<LoginForm />} />
+              <Route path="/register" element={<Signup />} />
+            </Routes>
+          </Router>
+        </AuthProvider>
+      )}
+    </>
+  );
 }
 
 export default App;
